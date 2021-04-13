0f6f78e994ec5671395c0eb8de60c2aca9266548
Taynara <tay.sene@gmail.com>
2021-04-13 01:31:15 -0300

    adiciona metodo get persistido com teste

--- app/src/controllers/cupons.py
+++ app/src/controllers/cupons.py
@@ -1,52 +1,16 @@
-from flask import Flask
-from flask_restplus import Api, Resource, abort
+from flask import Flask, request
 from src.server.instance import server
+from src.models.cupons import Cupom, cupons_schema
+from flask.json import jsonify
 
-app, api = server.app, server.api
+app, db = server.app, server.db
 
-cupons_db =   [ 
-                    { 
-                        "Cupons" :[
-                            {
-                                "id":1, 
-                                "tag": "Desconto", 
-                                "discount": "5", 
-                                "type":"%",
-                                "enabled_products": [1,2,3]
-                            }
-                        ]   
-                    }
-                ]
+class Cupons():
 
-@api.route('/cupons')
-class Cupons(Resource):
-
-    def get(self,):
-        return cupons_db
-    
-    def post(self,):
-        try:
-            response = api.payload
-            if Cupons.cupom_id_validate(self,response['id']) and Cupons.cupom_tag_validate(self,response['tag']):
-                cupons_db[0]['Cupons'].append(response)
-                return {"id": response['id'], "message": "Sucesso", "code": "200"}, 200
-            return abort(500)
-        except:
-            return abort(500, message='Solicitação inválida, verifique o payload enviado')
-
-    def cupom_id_validate(self,cupon_id):
-        cupons = cupons_db[0]['Cupons']
-        for item in cupons:
-            if item['id'] == cupon_id :  
-                return False 
-        return True
-    
-    def cupom_tag_validate(self,cupon_tag):
-        cupons = cupons_db[0]['Cupons']
-        for item in cupons:
-            if item['tag'] == cupon_tag :  
-                return False 
-        return True
-
+    @app.route('/cupons')
+    def index_cupom():
+        cupons = Cupom.query.all()
+        result = cupons_schema.dump(cupons)
+        return jsonify({'Cupons': result })
 
 cupons = Cupons()
--- app/src/tests/test_cupom.py
+++ app/src/tests/test_cupom.py
@@ -3,34 +3,10 @@ import routes
 import json
 from random import randrange
 
-
-
 class TestCupom:
 
     def test_get_cupons_response_code_200(self):
         response = requests.get(routes.CUPONS)
         assert response.status_code == 200
-    
-    def test_return_products_object_list(self):
-        response = requests.get(routes.CUPONS).json()
-        assert type(response) == type([])
-    
-    def test_create_cupom(self):
-        payload = { 'id':randrange(0, 1000, 2),'tag': 'Cupom'+ str(randrange(0, 1000, 2)), 'discount': '5', 'type': '%', 'enabled_products': [1,2,3]}
-        headers = {'content-type': 'application/json'}
-        response = requests.post(routes.CUPONS, data=json.dumps(payload), headers=headers)
-        assert response.status_code == 200
-    
-    def test_create_cupom_with_duplicated_tag(self):
-        payload = { 'id':randrange(0, 1000, 2),'tag': 'Cupom', 'discount': '5', 'type': '%', 'enabled_products': [1,2,3]}
-        headers = {'content-type': 'application/json'}
-        response = requests.post(routes.CUPONS, data=json.dumps(payload), headers=headers)
-        assert response.status_code == 500
-
-    def test_return_error_with_duplicated_id(self):
-        payload = {'id':'1', 'name':'Teste', 'price': 50.00, 'stock': 50 }
-        headers = {'content-type': 'application/json'}
-        response = requests.post(routes.PRODUCTS, data=json.dumps(payload), headers=headers)
-        assert response.status_code == 500
     
 
